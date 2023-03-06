64eb912e58fb3d7ec9a1167825f72f3e672b0a91
dwaynediddy <burrows.dwayne@icloud.com>
2023-03-06 12:09:42 +0100

    add more UI to welcome screen and assets

--- monochromonsStore-ios/view/welcomeView.swift
+++ monochromonsStore-ios/view/welcomeView.swift
@@ -10,12 +10,16 @@ import SwiftUI
 struct welcomeView: View {
     
     let name: String
+    @State private var typedText: String = "welcome to the store I am Monochromon, I own this store. I heard you want to earn some cash and prove you got salesman skills! sure thing"
     
     var body: some View {
         VStack {
             Text("Welcome, \(name)!")
                 .padding(.top, 50)
                 .font(.title)
+            Image("monochromon")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
             Spacer()
             ZStack {
                 Rectangle()
@@ -24,11 +28,25 @@ struct welcomeView: View {
                     .cornerRadius(10)
                     .overlay(RoundedRectangle(cornerRadius: 10)
                         .stroke(Color.black, lineWidth: 2))
-                Text("welcome to the store")
+                Text(typedText)
+                    .font(.body)
+                Button ("Next") {
+                    // go to next dialogue screen
+                }
             }
         }
         .navigationBarBackButtonHidden(true)
         .padding()
+//        .onAppear{
+//            let timer = Timer.publish(every: 0.1, on: .main, in: .common)
+//            let publisher = timer.autoconnect()
+//            let characters = Array("Welcome to the store")
+//            _ = publisher
+//                .zip(characters.publisher)
+//                .map { $0.1 }
+//                .sink(receiveValue: { self.typedText += $0 })
+//
+//        }
     }
 }
 
