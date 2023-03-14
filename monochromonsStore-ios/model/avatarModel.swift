600b1c5d2d7af3ac4724552003f7b2c07d0d7f10
dwaynediddy <burrows.dwayne@icloud.com>
2023-03-14 16:35:49 +0100

    cleaning up avatar model, neither model is probably needed.

--- monochromonsStore-ios/model/avatarModel.swift
+++ monochromonsStore-ios/model/avatarModel.swift
@@ -4,20 +4,11 @@
 //
 //  Created by Dwayne on 7/3/2023.
 //
-
-import Foundation
-
+//
+//import Foundation
+//
 struct Avatar {
     let name: String
     let image: String
 }
 
-class AvatarModel: ObservableObject {
-    @Published var avatars: [Avatar] = [
-           Avatar(name: "Gabumon", image: "gabumon"),
-           Avatar(name: "Agumon", image: "agumon"),
-           Avatar(name: "Petamon", image: "petamon"),
-           Avatar(name: "Polomon", image: "polomon"),
-           Avatar(name: "Biromon", image: "biromon"),
-       ]
-}
